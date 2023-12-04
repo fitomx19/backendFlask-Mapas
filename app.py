1d0e3c5b83cda32346cf327033dc421f2091cd92
Adolfo Huerta <fitox.metallica@gmail.com>
2023-12-03 23:17:19 -0600

    avance importante

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from .routes.mapas_routes import mapas_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(mapas_bp)
 
--- services/mapas_services.py
+++ services/mapas_services.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
-
+import json
 class MapasService:
     @staticmethod
     def obtener_mensaje():
@@ -11,16 +11,69 @@ class MapasService:
 class MapasGeoJsonService:
     @staticmethod
     def obtener_geojson(csv_file):
+        print("Hola Mundo desde Flask con MVC")
         if csv_file is None:
             return jsonify({'geojson_data': 'No hay archivo'})
         if csv_file == '1':
             nombre_archivo = './csv_mapas/inmuebles24Corregido.csv'
+        elif csv_file == '2':
+            nombre_archivo = './csv_mapas/ids_ut_raw.csv'
+        elif csv_file == '3':
+            nombre_archivo = './csv_mapas/michelada_31_10_23.csv'
+        elif csv_file == '4':
+            nombre_archivo = './csv_mapas/output2.geojson'
+            with open(nombre_archivo, 'r') as file:
+                geojson_data = file.read()
+                geojson_data = json.loads(geojson_data)
+
+                return jsonify({'geojson_data': geojson_data})
+
+
         geojson_data = MapasGeoJsonService.convertir_csv_a_geojson(nombre_archivo)
-        return jsonify({'geojson_data': geojson_data})
-    
+        return jsonify({'geojson_data': json.loads(geojson_data)})
+
+    @staticmethod
     def convertir_csv_a_geojson(csv_file):
         df = pd.read_csv(csv_file)
+        print(df.head(3))
         geometry = [Point(xy) for xy in zip(df['longitud'], df['latitud'])]
         gdf = gpd.GeoDataFrame(df, geometry=geometry, crs='EPSG:4326')
         geojson_data = gdf.to_crs('EPSG:4326').to_json()
+        print(type(geojson_data))
         return geojson_data
+    
+
+
+
+""" class MapasGeoJsonService:
+    @staticmethod
+    def obtener_geojson(csv_file):
+        print("Hola Mundo desde Flask con MVC")
+        if csv_file is None:
+            return jsonify({'geojson_data': 'No hay archivo'})
+        if csv_file == '1':
+            nombre_archivo = './csv_mapas/inmuebles24Corregido.csv'
+        elif csv_file == '2':
+            nombre_archivo = './csv_mapas/ids_ut_raw.csv'
+        elif csv_file == '3':
+            nombre_archivo = './csv_mapas/michelada_31_10_23.csv'
+        elif csv_file == '4':
+            nombre_archivo = './csv_mapas/output.geojson'
+        else:
+            return jsonify({'geojson_data': 'Archivo no válido'})
+
+        with open(nombre_archivo, 'r') as file:
+            geojson_data = file.read()
+            geojson_data = json.loads(geojson_data)
+
+        return jsonify({'geojson_data': geojson_data})
+
+    @staticmethod
+    def convertir_csv_a_geojson(csv_file):
+        df = pd.read_csv(csv_file)
+        
+        geometry = [Point(xy) for xy in zip(df['longitud'], df['latitud'])]
+        gdf = gpd.GeoDataFrame(df, geometry=geometry, crs='EPSG:4326')
+        geojson_data = gdf.to_crs('EPSG:4326').to_json()
+        print(type(geojson_data))
+        return geojson_data """
